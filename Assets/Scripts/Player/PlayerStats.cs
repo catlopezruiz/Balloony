ce9ba1bb7d884d9f3ebf2a900a81d0d3700b3431
catlopezruiz <catlopezruiz17@gmail.com>
2026-04-15 19:41:23 -0700

    Balanced player speed by adding max speed cap and reducing speed scaling

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,6 +4,7 @@ public class PlayerStats : MonoBehaviour
 {
     public int explosionRange = 1;
     public int maxBombs = 1;
+    public float maxSpeed = 8f;
 
     private PlayerMovement playerMovement;
     private BombPlacement bombPlacement;
@@ -20,22 +21,22 @@ public class PlayerStats : MonoBehaviour
     }
 
     public void IncreaseSpeed(float amount)
-{
-    PlayerMovement p1 = GetComponent<PlayerMovement>();
-    Player2Movement p2 = GetComponent<Player2Movement>();
-
-    if (p1 != null)
     {
-        p1.moveSpeed += amount;
-        Debug.Log("P1 Speed is now: " + p1.moveSpeed);
-    }
+        PlayerMovement p1 = GetComponent<PlayerMovement>();
+        Player2Movement p2 = GetComponent<Player2Movement>();
 
-    if (p2 != null)
-    {
-        p2.moveSpeed += amount;
-        Debug.Log("P2 Speed is now: " + p2.moveSpeed);
+        if (p1 != null)
+        {
+            p1.moveSpeed = Mathf.Min(p1.moveSpeed + amount, maxSpeed);
+            Debug.Log("P1 Speed is now: " + p1.moveSpeed);
+        }
+
+        if (p2 != null)
+        {
+            p2.moveSpeed = Mathf.Min(p2.moveSpeed + amount, maxSpeed);
+            Debug.Log("P2 Speed is now: " + p2.moveSpeed);
+        }
     }
-}
 
     public void IncreaseRange(int amount)
     {
