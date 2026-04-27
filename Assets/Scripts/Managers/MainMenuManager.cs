b062c19201c4b5289bb7eb111a4d026fcbf8ec73
catlopezruiz <catlopezruiz17@gmail.com>
2026-04-26 23:32:15 -0700

    finished main menu

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -4,16 +4,66 @@ public class MainMenuManager : MonoBehaviour
 {
     public GameObject titlePanel;
     public GameObject modePanel;
+    public GameObject vsAIPanel;
+    public GameObject localPlayPanel;
 
-    public void ShowModePanel()
+    public GameObject comingSoonText;
+
+    void Start()
     {
-        titlePanel.SetActive(false);
-        modePanel.SetActive(true);
+        ShowTitlePanel();
+
+        if (comingSoonText != null)
+            comingSoonText.SetActive(false);
     }
 
     public void ShowTitlePanel()
     {
         titlePanel.SetActive(true);
         modePanel.SetActive(false);
+        vsAIPanel.SetActive(false);
+        localPlayPanel.SetActive(false);
+
+        if (comingSoonText != null)
+            comingSoonText.SetActive(false);
+    }
+
+    public void ShowModePanel()
+    {
+        titlePanel.SetActive(false);
+        modePanel.SetActive(true);
+        vsAIPanel.SetActive(false);
+        localPlayPanel.SetActive(false);
+
+        if (comingSoonText != null)
+            comingSoonText.SetActive(false);
+    }
+
+    public void ShowVsAIPanel()
+    {
+        titlePanel.SetActive(false);
+        modePanel.SetActive(false);
+        vsAIPanel.SetActive(true);
+        localPlayPanel.SetActive(false);
+
+        if (comingSoonText != null)
+            comingSoonText.SetActive(false);
+    }
+
+    public void ShowLocalPlayPanel()
+    {
+        titlePanel.SetActive(false);
+        modePanel.SetActive(false);
+        vsAIPanel.SetActive(false);
+        localPlayPanel.SetActive(true);
+
+        if (comingSoonText != null)
+            comingSoonText.SetActive(false);
+    }
+
+    public void ShowComingSoon()
+    {
+        if (comingSoonText != null)
+            comingSoonText.SetActive(true);
     }
 }
